cb68acace9f8c62f9d9cf934243fdf8925fb6d06
AnshBhatti <44926412+AnshBhatti@users.noreply.github.com>
2020-02-08 17:50:05 -0500

    Update Segment_Tree.cpp

--- Segment_Tree.cpp
+++ Segment_Tree.cpp
@@ -2,48 +2,34 @@
 #include <algorithm>
 using namespace std;
 
-int arr[10000], n;
-int ar[20000];
-
-int q[2];
-int num, num1, r, s1;
+int arr[10000], ar[20000], n, num, s1;
 int max_i = 0;
 
 void segment_tree(int i, int n1, int n2) {
 	if (n1 != n2) {
 		int s = (n1 + n2) / 2;
 		num = *min_element(arr + n2, arr + n1 + 1);
-		num1 = *min_element(arr + s + 1, arr + n1);
-		for (int a = 0; a < n; a++) {
-			if (num == arr[a]) {
+		for (int a = 0; a < n; a++)
+			if (num == arr[a])
 				ar[i] = a;
-			}
-		}
 		segment_tree((i + 1) * 2 - 1, s, n2);
-		s = (n1 + n2) / 2;
 		segment_tree((i + 1) * 2, n1, s + 1);
 	}
 	else {
 		ar[i] = n1;
-		if (i > max_i) {
+		if (i > max_i)
 			max_i = i;
-		}
 	}
 }
 
 int main() {
 	cin >> n;
-	fill_n(ar, 20000, -1);
-	for (int i = 0; i < n; i++) {
+	fill_n(ar, 20000, -1); // Initializes all values to -1, so after the tree is created, -1 will show unused indexes
+	for (int i = 0; i < n; i++)
 		cin >> arr[i];
-	}
-	for (int i = 0; i < 2; i++) {
-		cin >> q[i];
-	}
 	num = *min_element(arr, arr + n);
 	segment_tree(0, n - 1, 0);
-	for (int i = 0; i <= max_i; i++) {
-		cout << ar[i] << ' ';
-	}
+	for (int i = 0; i <= max_i; i++)
+		cout << ar[i] << ' '; // Array representation is such that for every ar[n], it's left child is at ar[2*n] and right child at ar[2*n+1], like any other binary tree would be represented
 	cout << endl;
 }
